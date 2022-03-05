05ced1e43d6b538c4c165016cb047f4bfd0b7c7c
Huchan1124 <garlic1124@gmail.com>
2022-03-05 20:32:25 +0800

    完成表單驗證與送出

--- cart.js
+++ cart.js
@@ -1,107 +1,146 @@
-import { createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
-
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'alicia';
 
 
-const app = createApp({
-    data(){
+const app = Vue.createApp({
+    data() {
         return {
-            products:[],
-            cartData:{},
+            products: [],
+            cartData: {},
             productId: '',
-            isLoadingItem:'',
+            isLoadingItem: '',
+            form: {
+                "user": {
+                  "name": '',
+                  "email": '',
+                  "tel": '',
+                  "address": ''
+                },
+                "message": ''
+              },
         }
     },
-    methods:{
-        getProducts(){
+    methods: {
+        getProducts() {
             axios.get(`${apiUrl}/api/${apiPath}/products/all`)
-            .then((res) => {
-              this.products = res.data.products;
-            })
-            .catch((error) => {
-            })
+                .then((res) => {
+                    this.products = res.data.products;
+                })
+                .catch((error) => {
+                })
         },
-        openProductModal(id){
+        openProductModal(id) {
             this.productId = id;
             this.$refs.productModal.openModal()
         },
-        getCart(){
+        getCart() {
             axios.get(`${apiUrl}/api/${apiPath}/cart`)
-            .then((res) => {
-              this.cartData = res.data.data;
-            })
-            .catch((error) => {
-            })
+                .then((res) => {
+                    this.cartData = res.data.data;
+                })
+                .catch((error) => {
+                })
 
         },
-        addToCart(id,qty = 1){
+        addToCart(id, qty = 1) {
             const postData = {
                 "data": {
-                  "product_id": id,
-                  qty,
+                    "product_id": id,
+                    qty,
                 }
-              };
+            };
 
             this.isLoadingItem = id;
 
             axios.post(`${apiUrl}/api/${apiPath}/cart`, postData)
-            .then((res) => {
-                this.getCart();
-                this.$refs.productModal.closeModal()
-                this.isLoadingItem = '';
-            })
-            .catch((error) => {
-            })
-        },
-        removeCartItem(id){
-
-            this.isLoadingItem = id;
-            if(confirm("確定要移除品項?")){
-                axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
                 .then((res) => {
                     this.getCart();
+                    this.$refs.productModal.closeModal()
                     this.isLoadingItem = '';
                 })
                 .catch((error) => {
                 })
-            }
-         
         },
-        updateCartItem(cartItem){
-            
+        removeCartItem(id) {
+
+            this.isLoadingItem = id;
+            if (confirm("確定要移除品項?")) {
+                axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
+                    .then((res) => {
+                        this.getCart();
+                        this.isLoadingItem = '';
+                    })
+                    .catch((error) => {
+                    })
+            }
+
+        },
+        updateCartItem(cartItem) {
+
             const postData = {
                 "data": {
-                  "product_id": cartItem.id,
-                  "qty": cartItem.qty,
+                    "product_id": cartItem.id,
+                    "qty": cartItem.qty,
                 }
-              };
+            };
 
             this.isLoadingItem = cartItem.id;
 
             axios.put(`${apiUrl}/api/${apiPath}/cart/${cartItem.id}`, postData)
-            .then((res) => {
-                this.isLoadingItem = '';
-                this.getCart();
-            })
-            .catch((error) => {
-            })
-
-        },
-        removeAllCart(){
-            if(confirm("確定要清空購物車?")){
-                axios.delete(`${apiUrl}/api/${apiPath}/carts`)
                 .then((res) => {
+                    this.isLoadingItem = '';
                     this.getCart();
                 })
                 .catch((error) => {
                 })
-            }
-         
-        },
 
-   
+        },
+        removeAllCart() {
+            if (confirm("確定要清空購物車?")) {
+                axios.delete(`${apiUrl}/api/${apiPath}/carts`)
+                    .then((res) => {
+                        this.getCart();
+                    })
+                    .catch((error) => {
+                    })
+            }
+
+        },
+        createOrder(){
+           const postData = {
+            "data": this.form
+          }
+
+           this.isLoadingItem = true;
+           
+           axios.post(`${apiUrl}/api/${apiPath}/order`, postData)
+           .then((res) => {
+               console.log(res)
+               this.$refs.form.resetForm()
+               this.isLoadingItem = '';
+               
+           })
+           .catch((error) => {
+           })
+        }
+        ,
+        isPhone(value) {
+            const phoneNumber = /^(09)[0-9]{8}$/;
+
+            if(value === undefined ){
+                return '手機號碼 為必填';
+            }
+
+            if(phoneNumber.test(value)){
+                return true
+            } else {
+                return '請輸入正確的手機號碼格式'
+            }
+          
+        }
+
+
 
 
 
@@ -110,57 +149,78 @@ const app = createApp({
 
         this.getProducts();
         this.getCart();
-        
+
     },
 });
 
-app.component('product-modal',{
-    template:'#userProductModal',
-    props:['id'],
-    data(){
-        return{
+app.component('product-modal', {
+    template: '#userProductModal',
+    props: ['id'],
+    data() {
+        return {
             // 相同作用域
-            modal:{},
-            product:{},
-            qty:1,
+            modal: {},
+            product: {},
+            qty: 1,
 
         }
     },
-    watch:{
-        id(){
-           this.getProduct() 
+    watch: {
+        id() {
+            this.getProduct()
         }
 
     },
-    methods:{
-        openModal(){
+    methods: {
+        openModal() {
             this.modal.show()
         },
-        closeModal(){
+        closeModal() {
             this.modal.hide()
         },
-        getProduct(){
+        getProduct() {
             axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
-            .then((res) => {
-              this.product = res.data.product;
-              
-            })
-            .catch((error) => {
-    
-            })
+                .then((res) => {
+                    this.product = res.data.product;
+
+                })
+                .catch((error) => {
+
+                })
         },
-        addToCart(){
-          this.$emit('add-cart',this.product.id,this.qty);
-          setTimeout(() =>
-           { this.qty = 1;}, 1000);
-         
+        addToCart() {
+            this.$emit('add-cart', this.product.id, this.qty);
+            setTimeout(() => { this.qty = 1; }, 1000);
+
         },
     },
-    mounted(){
-      this.modal = new bootstrap.Modal(this.$refs.modal);
-      
+    mounted() {
+        this.modal = new bootstrap.Modal(this.$refs.modal);
+
     },
 })
+
+// 加入全部規則
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+      VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+  });
+
+// i18n
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'), //語系設定
+    
+});
+
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
 app.mount('#app');
 
